603f42c4e39d4c238804b7b8e102ef4e278aa4c9
RathodBh <111rathodbhavesh@gmail.com>
2023-04-26 10:30:28 +0530

    show data and sorting done

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Add from './pages/Add';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <Add />
-  </React.StrictMode>
+  </>
 );
 
--- src/pages/ShowData.jsx
+++ src/pages/ShowData.jsx
@@ -1,31 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const columns = [
+    {
+        show:"Transaction date",
+        db:"transDate"
+    },
+    {
+        show: "Transaction type",
+        db:"transactionType"
+    },
+    {
+        show: "Transfer from",
+        db:"fromAccount"
+    },
+  
+    {
+        show: "Transfer to",
+        db:"toAccount"
+    },
+  
+    {
+        show: "amount",
+        db:"amount"
+    },
+    {
+        show: "Receipt",
+        db:"receipt"
+    },
+    {
+        show: "Notes",
+        db:"notes"
+    },
+];
 
 const ShowData = ({ data }) => {
+  const [tempData, setTempData] = useState([{}]);
+  const [sortMethod,setSortMethod] = useState(1)
+
+  useEffect(() => {
+    setTempData(data);
+  }, [data]);
+
+  const sort = (name) => {
+    let cloneData = [...data];
+    if(sortMethod > 2){
+        setSortMethod(prev=> 1)
+    }else{
+        setSortMethod(prev => sortMethod+1)
+    }
+    console.log("Before sorting",sortMethod)
+
+    if(sortMethod===1){
+        cloneData = cloneData.sort((a, b) => (a[name] > b[name] ? 1 : -1));
+    }else if(sortMethod===2){
+        cloneData = cloneData.sort((a, b) => (a[name] < b[name] ? 1 : -1));
+    }
+    setTempData(prev=>cloneData);
+    console.log("sorting",sortMethod)
+  };
   return (
     <>
       <table style={{ width: "100%" }} border={1}>
         <thead>
           <tr>
             <th>#</th>
-            <th>Transaction date</th>
-            <th>Transaction type</th>
-            <th>Transfer from</th>
-            <th>Transfer to</th>
-            <th>Amount</th>
-            <th>Receipt</th>
-            <th>Notes</th>
+            {columns.map((c, i) => (
+              <th key={i}>  
+                <span>{(sortMethod==1)? <>&#8661; </> : (sortMethod==2)? <>&#8657; </> : <>&#8659; </>}</span>
+                <span onClick={(e) => sort(columns[i].db)}>{columns[i].show}</span>
+              </th>
+            ))}
           </tr>
         </thead>
 
         <tbody>
-          {data?.map((d, i) => (
+          {tempData?.map((d, i) => (
             <tr key={i + 1}>
               <td>{i + 1}</td>
               <td>{d.transDate}</td>
               <td>{d.transactionType}</td>
               <td>{d.fromAccount}</td>
               <td>{d.toAccount}</td>
-              <td>{d.amount}</td>
+              <td>&#8377; {d.amount}</td>
               <td>
                 <img
                   src={d.receipt}
